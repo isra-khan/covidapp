54c9044692521176d8071b2a3ba03b7254bb1151
isra <israkhan.ik1@gmail.com>
2023-11-05 04:34:22 -0800

    code push

--- covidapp/lib/view/world_states.dart
+++ covidapp/lib/view/world_states.dart
@@ -50,139 +50,130 @@ class _WorldStateScreenState extends State<WorldStateScreen>
           SizedBox(
             height: MediaQuery.of(context).size.height * 0.01,
           ),
-          Obx(
-            () => FutureBuilder(
-                future: _servicesController.fetchWorkStateRecords(),
-                builder: (context, AsyncSnapshot<CasesModel> snapshot) {
-                  if (_servicesController.isLoading.value) {
-                    return Expanded(
-                        flex: 1,
-                        child: SpinKitFadingCircle(
-                          color: Colors.white,
-                          size: 50,
-                          controller: _controller,
-                        ));
-                  } else {
-                    return Column(
-                      children: [
-                        PieChart(
-                          dataMap: {
-                            "Total":
-                                double.parse(snapshot.data!.cases.toString()),
-                            "Recovered": double.parse(
-                                snapshot.data!.recovered.toString()),
-                            "Deaths":
-                                double.parse(snapshot.data!.deaths.toString())
-                          },
-                          animationDuration: const Duration(microseconds: 1200),
-                          chartValuesOptions: ChartValuesOptions(
-                              showChartValuesInPercentage: true),
-                          legendOptions: LegendOptions(
-                              legendPosition: LegendPosition.left),
-                          chartType: ChartType.ring,
-                          chartRadius: MediaQuery.of(context).size.width / 3.2,
-                          colorList: chartColors,
-                        ),
-                        SizedBox(
-                          height: MediaQuery.of(context).size.height * 0.05,
-                        ),
-                        Card(
-                          child: Padding(
-                            padding: EdgeInsets.only(
-                                left: 10, bottom: 5, right: 10, top: 10),
-                            child: Column(children: [
-                              ReusableRow(
-                                title: "Total",
-                                value: snapshot.data!.cases.toString(),
-                              ),
-                              SizedBox(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.05,
-                              ),
-                              ReusableRow(
-                                title: "Deaths",
-                                value: snapshot.data!.deaths.toString(),
-                              ),
-                              SizedBox(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.05,
-                              ),
-                              ReusableRow(
-                                title: "Recovered",
-                                value: snapshot.data!.recovered.toString(),
-                              ),
-                              SizedBox(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.05,
-                              ),
-                              ReusableRow(
-                                title: "Active",
-                                value: snapshot.data!.active.toString(),
-                              ),
-                              SizedBox(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.05,
-                              ),
-                              ReusableRow(
-                                title: "Critical",
-                                value: snapshot.data!.critical.toString(),
-                              ),
-                              SizedBox(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.05,
-                              ),
-                              ReusableRow(
-                                title: "Today Deaths",
-                                value: snapshot.data!.todayDeaths.toString(),
-                              ),
-                              SizedBox(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.05,
-                              ),
-                              ReusableRow(
-                                title: "Today Recovered",
-                                value: snapshot.data!.todayRecovered.toString(),
-                              ),
-                              SizedBox(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.05,
-                              ),
-                            ]),
-                          ),
-                        ),
-                        SizedBox(
-                          height: MediaQuery.of(context).size.height * 0.05,
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            // Navigator.push(
-                            //     context,
-                            //     MaterialPageRoute(
-                            //         builder: (context) =>
-                            //             CountriesListScreen()));
-                            Get.to(CountriesListScreen());
-                          },
-                          child: Container(
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 100, vertical: 15),
-                            decoration: BoxDecoration(
-                                color: Colors.green,
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10))),
-                            child: Text(
-                              "Track Countries",
-                              style: TextStyle(
-                                  fontSize: 14,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold),
+          FutureBuilder(
+              future: _servicesController.fetchWorkStateRecords(),
+              builder: (context, AsyncSnapshot<CasesModel> snapshot) {
+                if (_servicesController.isLoading.value) {
+                  return Expanded(
+                      flex: 1,
+                      child: SpinKitFadingCircle(
+                        color: Colors.white,
+                        size: 50,
+                        controller: _controller,
+                      ));
+                } else {
+                  return Column(
+                    children: [
+                      PieChart(
+                        dataMap: {
+                          "Total":
+                              double.parse(snapshot.data!.cases.toString()),
+                          "Recovered":
+                              double.parse(snapshot.data!.recovered.toString()),
+                          "Deaths":
+                              double.parse(snapshot.data!.deaths.toString())
+                        },
+                        animationDuration: const Duration(microseconds: 1200),
+                        chartValuesOptions: ChartValuesOptions(
+                            showChartValuesInPercentage: true),
+                        legendOptions:
+                            LegendOptions(legendPosition: LegendPosition.left),
+                        chartType: ChartType.ring,
+                        chartRadius: MediaQuery.of(context).size.width / 3.2,
+                        colorList: chartColors,
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.05,
+                      ),
+                      Card(
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              left: 10, bottom: 5, right: 10, top: 10),
+                          child: Column(children: [
+                            ReusableRow(
+                              title: "Total",
+                              value: snapshot.data!.cases.toString(),
                             ),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.05,
+                            ),
+                            ReusableRow(
+                              title: "Deaths",
+                              value: snapshot.data!.deaths.toString(),
+                            ),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.05,
+                            ),
+                            ReusableRow(
+                              title: "Recovered",
+                              value: snapshot.data!.recovered.toString(),
+                            ),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.05,
+                            ),
+                            ReusableRow(
+                              title: "Active",
+                              value: snapshot.data!.active.toString(),
+                            ),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.05,
+                            ),
+                            ReusableRow(
+                              title: "Critical",
+                              value: snapshot.data!.critical.toString(),
+                            ),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.05,
+                            ),
+                            ReusableRow(
+                              title: "Today Deaths",
+                              value: snapshot.data!.todayDeaths.toString(),
+                            ),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.05,
+                            ),
+                            ReusableRow(
+                              title: "Today Recovered",
+                              value: snapshot.data!.todayRecovered.toString(),
+                            ),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.05,
+                            ),
+                          ]),
+                        ),
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.05,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          // Navigator.push(
+                          //     context,
+                          //     MaterialPageRoute(
+                          //         builder: (context) =>
+                          //             CountriesListScreen()));
+                          Get.to(CountriesListScreen());
+                        },
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 100, vertical: 15),
+                          decoration: BoxDecoration(
+                              color: Colors.green,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10))),
+                          child: Text(
+                            "Track Countries",
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
                           ),
                         ),
-                      ],
-                    );
-                  }
-                }),
-          ),
+                      ),
+                    ],
+                  );
+                }
+              }),
         ]),
       ),
     ));
